f4d0d097054b19380c59d4afb8d771b274c3a45d
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2020-02-12 02:55:53 -0300

    First AI Prototype
    - HungerController: Controls the energy and the hunger status of the animal
    - IMovable: Interface that represents movable objects
    - Monkey: First Class that controls the behaviour of the monkey

---
+++ Project SimIA/Assets/Scripts/HungerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SurvivalControllers
+{
+    public class HungerController : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private int maxHunger = 10;
+        private int actualHunger;
+        private bool isHungry = true;
+
+        public Text text;
+
+        private void Start()
+        {
+            actualHunger = maxHunger;
+        }
+
+        private void Update()
+        {
+            if (isHungry)
+            {
+                StartCoroutine(HungerTimer());
+            }
+            if (actualHunger <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        public bool IsHungry()
+        {
+            if (actualHunger >= (maxHunger * 0.75f))
+                return false;
+            else
+                return true;
+        }
+
+        public void Eat()
+        {
+            print("Ate");
+            actualHunger += 4;
+        }
+
+        IEnumerator HungerTimer()
+        {
+            isHungry = false;
+            yield return new WaitForSecondsRealtime(2f);
+            actualHunger--;
+            text.text = "Energia: " + actualHunger;
+            isHungry = true;
+        }
+    }
+}
---
+++ Project SimIA/Assets/Scripts/Monkey.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SurvivalControllers;
+
+public class Monkey : MonoBehaviour, IMovable
+{
+    private HungerController hungerController;
+
+    [Header("Food Searching Attributes")]
+    [SerializeField] private LayerMask foodMask;
+    [SerializeField] private float viewRange;
+    private bool foundFood = false;
+    private GameObject food;
+
+    private Rigidbody myRigidbody;
+    [SerializeField] private float speed;
+
+    public Text text;
+
+    private void Awake()
+    {
+        hungerController = GetComponent<HungerController>();
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (hungerController.IsHungry())
+        {
+            text.text = "Faminto";
+
+            if (!foundFood)
+                SeachFood();
+            else
+                MoveToThis(food.transform.position);
+        }
+        else
+        {
+            text.text = "Saciado";
+        }
+
+    }
+
+    private void SeachFood()
+    {
+        Collider[] foodNextToThis = Physics.OverlapSphere(transform.position, viewRange, foodMask);
+        int numberOfFoods = foodNextToThis.Length;
+
+        if (numberOfFoods == 0)
+        {
+            foundFood = false;
+            print("Can't find food, gonna die!");
+        }
+        else
+        {
+            food = foodNextToThis[0].gameObject;
+            foundFood = true;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, viewRange);
+    }
+
+    public void MoveToThis(Vector3 destiny)
+    {
+        Vector3 direction = new Vector3(destiny.x - transform.position.x, 0, destiny.z - transform.position.z).normalized;
+        myRigidbody.velocity = direction * speed;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Food"))
+        {
+            hungerController.Eat();
+            foundFood = false;
+            Destroy(col.gameObject);
+        }
+    }
+}
