2638adfd0c796bb48b4aa2d21e293d663193bfec
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2020-05-31 19:40:33 -0300

    Menu better UI

---
+++ Project SimIA/Assets/Scripts/UserInterface/MouseParallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseParallax : MonoBehaviour
+{
+    private Vector3 pos;
+    private Vector3 startPos;
+
+    public float movementAmount;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        var pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        pos.z = 0;
+        transform.position = pos;
+        transform.position = new Vector3(startPos.x + (pos.x * movementAmount), startPos.y + (pos.y * movementAmount), 0);
+    }
+}
